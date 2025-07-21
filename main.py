8b7d6faf5576e2c2e72f5aa03922d4e63f8b42cf
曾斌 <zengbin@xiaohongshu.com>
2025-07-21 17:17:39 +0800

    feat: 测试自动化,名字更改

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from astrbot.api import logger
 from astrbot.api.message_components import Plain, Image
 from astrbot.api.all import AstrBotConfig
 
-@register("xhs_auto", "your_name", "每日自动小红书推广", "1.0.0", "")
+@register("xhs_demo", "your_name", "每日自动小红书推广", "1.0.0", "")
 class XhsAutoPush(Star):
     def __init__(self, ctx: Context, cfg: AstrBotConfig):
         super().__init__(ctx)
